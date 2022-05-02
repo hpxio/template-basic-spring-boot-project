ac27cca323e4623ff3851f1bd5a8793a4e104ab9
HPxIO <hpx.io@keemail.me>
2022-05-03 01:15:48 +0530

    init-project

---
+++ src/main/java/com/app/hpx/springtemplate/TemplateBasicSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.app.hpx.springtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateBasicSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateBasicSpringBootProjectApplication.class, args);
+	}
+
+}
